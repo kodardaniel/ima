9cde2e5709048d531ea91cf26c47ff398e950129
kodardaniel <63422092+kodardaniel@users.noreply.github.com>
2020-04-11 22:01:48 +0200

    Autopilot
    - Disabled firstperson control
    - Added autopilot control (NavMesh)

---
+++ Assets/Scripts/Player Control/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public Transform world;
+
+    private float halfWalkDistance;
+    private Vector3 destination;
+    private NavMeshAgent agent;
+
+    void Start()
+    {
+        halfWalkDistance = 0.9f * world.lossyScale.x / 2;
+        agent = gameObject.GetComponent(typeof(NavMeshAgent)) as NavMeshAgent;
+        newDestination();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown("return"))
+        {
+            newDestination();
+        }
+
+        // Check if we've reached the destination
+        if (agent.remainingDistance <= 4f)
+        {
+            newDestination();
+        }
+    }
+
+    void newDestination()
+    {
+        destination = new Vector3(Random.Range(-halfWalkDistance, halfWalkDistance), 0, Random.Range(-halfWalkDistance, halfWalkDistance));
+        agent.SetDestination(destination);
+    }
+}
