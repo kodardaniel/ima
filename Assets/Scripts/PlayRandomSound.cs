6131dbaf49ca765c54760355a3b89175a5204a20
kodardaniel <63422092+kodardaniel@users.noreply.github.com>
2020-04-23 22:56:43 +0200

    Add Random Sound Object

---
+++ Assets/Scripts/PlayRandomSound.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayRandomSound : MonoBehaviour
+{
+    
+    public AudioClip[] audioClipArray;
+
+    private AudioSource _as;
+
+    void Awake()
+	{
+        _as = GetComponent<AudioSource>();
+	}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayRandom();
+    }
+
+    private void Update()
+    {
+       if (_as.isPlaying == false)
+        {
+            PlayRandom();
+        }
+    }
+
+    void PlayRandom()
+    {
+        _as.clip = audioClipArray[Random.Range(0, audioClipArray.Length)];
+        _as.PlayOneShot(_as.clip);
+    }
+}
