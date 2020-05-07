45c0e6363e72db523bfa8e0273aa6fc8a9a0c34a
Daniel <daniel@oceanobservations.com>
2020-05-07 23:28:23 +0200

    Update Random Sound Object
    - Remove old viz
    - Add Amplitude

--- Assets/Scripts/PlayRandomSound.cs
+++ Assets/Scripts/PlayRandomSound.cs
@@ -1,48 +1,1 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[RequireComponent(typeof(AudioSource))]
-public class PlayRandomSound : MonoBehaviour
-{
-    
-    public AudioClip[] audioClipArray;
-    public float duration;
-    public AudioDataVisSingle Viz;
-
-    private AudioSource audioSource;
-
-
-    void Awake()
-	{
-        audioSource = GetComponent<AudioSource>();
-	}
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        RandomizeClip();
-        PlayClip();
-    }
-
-    void RandomizeClip()
-    {
-        audioSource.clip = audioClipArray[Random.Range(0, audioClipArray.Length)];
-    }
-
-    void PlayClip()
-    {  
-        audioSource.PlayOneShot(audioSource.clip);
-        StartCoroutine(PrepNextClip());
-        Viz.updateAudioSource();
-    }
-
-    // Coroutine for prepping next clip
-    IEnumerator PrepNextClip()
-    {
-        RandomizeClip();
-        duration = audioSource.clip.length;
-        yield return new WaitForSecondsRealtime(duration);
-        PlayClip();
-    }
-}
+﻿using System.Collections;using System.Collections.Generic;using UnityEngine;[RequireComponent(typeof(AudioSource))]public class PlayRandomSound : MonoBehaviour{        public AudioClip[] audioClipArray;    public float duration;    private AudioSource audioSource;    void Awake()	{        audioSource = GetComponent<AudioSource>();	}    // Start is called before the first frame update    void Start()    {        RandomizeClip();        PlayClip();    }    void RandomizeClip()    {        audioSource.clip = audioClipArray[Random.Range(0, audioClipArray.Length)];    }    void PlayClip()    {          audioSource.PlayOneShot(audioSource.clip);        StartCoroutine(PrepNextClip());    }    // Coroutine for prepping next clip    IEnumerator PrepNextClip()    {        RandomizeClip();        duration = audioSource.clip.length;        yield return new WaitForSecondsRealtime(duration);        PlayClip();    }}
