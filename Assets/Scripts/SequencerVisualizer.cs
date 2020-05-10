55b24098678a6560d77492c26d7934454531fd46
kodardaniel <63422092+kodardaniel@users.noreply.github.com>
2020-05-10 16:10:29 +0200

    Create SequencerVisualizer.cs

---
+++ Assets/Scripts/SequencerVisualizer.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;using CrazyMinnow.AmplitudeWebGL;
+
+public class SequencerVisualizer : MonoBehaviour
+{
+    public Amplitude amp;    public float multiplier = 10f;    private Material material;    private Color emissionColor;    private float intensity;
+
+    void Start()    {        material = GetComponent<MeshRenderer>().materials[0];        emissionColor = material.GetColor("_EmissionColor");    }    void Update()    {        //intensity = amp.average * multiplier;        //material.SetColor("_EmissionColor", emissionColor * intensity);    }
+}
