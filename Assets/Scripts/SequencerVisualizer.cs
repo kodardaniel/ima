54897e1702678ffbd5da183824c5831aeb37e77c
kodardaniel <63422092+kodardaniel@users.noreply.github.com>
2020-05-10 17:30:11 +0200

    Update Sequencer Object.prefab
    Add visualization with Amplitude

--- Assets/Scripts/SequencerVisualizer.cs
+++ Assets/Scripts/SequencerVisualizer.cs
@@ -2,7 +2,20 @@
 
 public class SequencerVisualizer : MonoBehaviour
 {
-    public Amplitude amp;    public float multiplier = 10f;    private Material material;    private Color emissionColor;    private float intensity;
+    public Amplitude[] amp;    public float multiplier = 10f;    public float updateStep = 0.1f;    public float loudest;    private float currentUpdateTime = 0f;    private Material material;    private Color emissionColor;    private float intensity;
 
-    void Start()    {        material = GetComponent<MeshRenderer>().materials[0];        emissionColor = material.GetColor("_EmissionColor");    }    void Update()    {        //intensity = amp.average * multiplier;        //material.SetColor("_EmissionColor", emissionColor * intensity);    }
+    void Start()    {        amp = GetComponentsInChildren<Amplitude>();        material = GetComponent<MeshRenderer>().materials[0];        emissionColor = material.GetColor("_EmissionColor");    }    void Update()    {        currentUpdateTime += Time.deltaTime;        if (currentUpdateTime >= updateStep)
+		{
+            currentUpdateTime = 0f;
+            loudest = 0f;
+            for (int i = 0; i < amp.Length; ++i)
+            {
+                if (amp[i].average > loudest)
+                {
+                    loudest = amp[i].average;
+                }
+            }
+            intensity = loudest * multiplier;
+            material.SetColor("_EmissionColor", emissionColor * intensity);
+        }           }
 }
