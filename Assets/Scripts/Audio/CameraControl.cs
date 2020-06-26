d6f0404f65567e8ca7ffd9eed5b9e8ce226604a3
kodardaniel <63422092+kodardaniel@users.noreply.github.com>
2020-06-26 18:51:04 +0200

    Change to Camera as child of Player instead of EzCamera
    Make player look at target

---
+++ Assets/Scripts/Audio/CameraControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+
+    GameObject player;
+    Vector3 cameraOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        cameraOffset = new Vector3(0, 2, -6);
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + cameraOffset;
+        transform.forward = player.transform.forward;
+    }
+}
--- Assets/Scripts/Player Control/AutoPilot.cs
+++ Assets/Scripts/Player Control/AutoPilot.cs
@@ -32,6 +32,7 @@ public class AutoPilot : MonoBehaviour
         // Move our position a step closer to the target.
         float step = speed * Time.deltaTime; // calculate distance to move
         transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+        transform.LookAt(target.position);
 
         // Check if the position of the cube and sphere are approximately equal.
         if (Vector3.Distance(transform.position, target.position) < 0.001f)
